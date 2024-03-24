5174b06539781b63511dbff2d859c39900972699
DinuliJay <123982235+DinuliJay@users.noreply.github.com>
2024-03-24 11:17:03 +0530

    Delete Final Backend directory

--- Final Backend/Database Connection final/api/ApiHandler.py
+++
@@ -1,8 +0,0 @@
-# from flask_restful import Resource
-
-# class ApiHandler(Resource):
-#     def get(self):
-#         return {
-#             'resultstatus': 'SUCCESS',
-#             'message': "Hello Api Handler.py"
-#         }
